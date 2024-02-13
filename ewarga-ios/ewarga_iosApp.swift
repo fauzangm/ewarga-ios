25936eaf5ee67fa399c9344c9d0879c8f30a9556
Fauzan Ghozi Mubarak <fauzanghozi56@gmail.com>
2024-02-13 08:47:24 +0700

    initial

---
+++ ewarga-ios/ewarga_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ewarga_iosApp.swift
+//  ewarga-ios
+//
+//  Created by Fauzan Ghozi Mubarak on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ewarga_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
